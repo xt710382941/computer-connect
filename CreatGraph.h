5a615dae628119d9a9380fa2e635419faaecc0c7
Tisword <710382941@qq.com>
2018-12-05 12:51:56 +0800

    1

---
+++ CreatGraph.h
@@ -0,0 +1,20 @@
+//#include "SeqList.h"
+#define MaxVertices 100
+#define MaxWeight 10000
+#include <malloc.h>
+//#include "GraphInitiate.h"
+//#include "InsertEdge.h"
+//#include "InsertVertex.h"
+void CreatGraph(AdjMGraph *G,DataType v[],int n,RowColWeight W[],int e)
+{
+	int i,k;
+	
+	GraphInitiate(G);
+	for(i=0;i<n;i++)
+	{
+			//cout<<n<<endl;
+			InsertVertex(G,v[i]);
+	}
+	for(k=0;k<e;k++)
+        InsertEdge(G,W[k].row,W[k].col,W[k].weight);
+}
